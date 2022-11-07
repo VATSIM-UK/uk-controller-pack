dd567a0a46ef5f0343dd9c31a22a35588117dab2
Samuel Lefevre <64741876+SamLefevre@users.noreply.github.com>
2022-11-07 20:08:20 +0000

    Create Logon Details.py

---
+++ UK/Logon Details.py
@@ -0,0 +1,50 @@
+# Enables clearing screen
+
+import os
+import glob
+from time import sleep
+
+# User Inputs
+
+Name = input("Enter Name: ")
+os.system('cls')
+CID = input("Enter CID: ")
+os.system('cls')
+#Password = input("Enter Password: ")
+#os.system('cls')
+# Left commented as password is stored plain text, could be uncommented later
+
+CPDLC = input("Enter Hoppie's Code: ")
+os.system('cls')
+
+# Write CPDLC to TopSky
+
+f = open("Data/Plugin/TopSky/TopSkyCPDLChoppieCode.txt", "w")
+f.write(CPDLC)
+f.close
+
+# Add CID as Nickname to VCCS profiles
+
+PrfVCCS=("TeamSpeakVccs	Ts3NickName	"+CID)
+
+# Appeding "LastSession" to user input
+
+PrfName=("LastSession	realname	"+Name)
+PrfCID=("LastSession	certificate	"+CID)
+#PrfPassword=("LastSession	password	"+Password)
+
+# Adds all .prf files to an array and then writes to all those files
+
+aerodrome = (glob.glob('**/*.prf'))
+
+for i in aerodrome:
+    f=open(i, "a")
+    f.write(PrfName)
+    f.write("\n")
+    f.write(PrfCID)
+    f.write("\n")
+    f.write(PrfVCCS)
+    f.close
+
+print("Detail entry process complete")
+anykey=input("Press enter to close")
