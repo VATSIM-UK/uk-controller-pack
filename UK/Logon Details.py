6045891b9c281cced655db9741c2310749db4224
luke11brown <13762210+luke11brown@users.noreply.github.com>
2023-09-15 14:55:23 +0400

    comment C2, I2 not used

--- UK/Logon Details.py
+++ UK/Logon Details.py
@@ -10,7 +10,7 @@ os.system('cls')
 CID = input("Enter CID: ")
 os.system('cls')
 #User selects controller rating from list
-options = ['OBS', 'S1', 'S2', 'S3', 'C1', 'C2', 'C3',  'I1', 'I2', 'I3', 'SUP', 'ADM']
+options = ['OBS', 'S1', 'S2', 'S3', 'C1', 'C2 (not used)', 'C3',  'I1', 'I2 (not used)', 'I3', 'SUP', 'ADM']
 
 rating = ''
 
