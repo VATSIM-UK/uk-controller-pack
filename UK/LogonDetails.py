994d1b62eec4be00ff199b973ad3593c6f2461c7
luke11brown <13762210+luke11brown@users.noreply.github.com>
2023-09-17 09:12:48 +0400

    Update LogonDetails.py (adds rating, OBS callsign)

--- UK/LogonDetails.py
+++ UK/LogonDetails.py
@@ -2,15 +2,35 @@ import os
 import time
 
 # User Inputs
-
+# Name
 Name = input("Enter Name: ")
 os.system('cls')
+#Initials (used for ESE alternate ownership and OBS callsign)
+Initials = input("Enter Initials (for _OBS callsign):")
+os.system('cls')
+# VATSIM Certificate ID
 CID = input("Enter CID: ")
+os.system('cls')
+#Controller Rating
+options = ['OBS', 'S1', 'S2', 'S3', 'C1', 'C2 (not used)', 'C3',  'I1', 'I2 (not used)', 'I3', 'SUP', 'ADM']
+
+rating = ''
+
+input_message = "Select Controller Rating:\n"
+
+for index, item in enumerate(options):
+    input_message += f'{index}) {item}\n'
+
+input_message += 'Your choice: '
+
+while rating not in map(str, range(0, len(options))):
+    rating = input(input_message)
 os.system('cls')
 #Password = input("Enter Password: ")
 #os.system('cls')
 # Left commented as password is stored plain text, could be uncommented later
 
+#Hoppie CPDLC logon code
 CPDLC = input("Enter Hoppie's Code: ")
 os.system('cls')
 
@@ -22,12 +42,18 @@ f.close
 
 # Add CID as Nickname to VCCS profiles
 
-PrfVCCS=("TeamSpeakVccs	Ts3NickName	"+CID)
+PrfVCCS=("TeamSpeakVccs Ts3NickName	"+CID)
 
-# Appeding "LastSession" to user input
+#Set intials as object
+
+EseInitials=(Initials)
+
+# Prefixing "LastSession" to user input
 
 PrfName=("LastSession	realname	"+Name)
 PrfCID=("LastSession	certificate	"+CID)
+Prfrating=("LastSession	rating	"+rating)
+PrfOBSCallsign=("LastSession callsign	"+EseInitials+"_OBS")
 #PrfPassword=("LastSession	password	"+Password)
 
 # Adds all .prf files to an array and then writes to all those files
@@ -37,12 +63,19 @@ for root, dirs, files in os.walk(os.getcwd()):
         if file.endswith(".prf"):
             file_path = os.path.join(root, file)
             with open(file_path, 'a') as f:
-                f.write(f"\n{PrfName}\n{PrfCID}\n")
+                f.write(f"\n{PrfName}\n{PrfCID}\n{PrfOBSCallsign}\n{Prfrating}\n{PrfVCCS}\n")
         elif file.endswith("Plugins.txt"):
             file_path = os.path.join(root, file)
             with open(file_path, 'r') as f:
                 line = f.read()
                 line = line.replace("END", "vSMR:cpdlc_password:"+CPDLC+"\nEND")
+                writeFile = open(file_path, "w")
+                writeFile.write(line)
+        elif file.endswith(".ese"):
+            file_path = os.path.join(root, file)
+            with open(file_path, 'r') as f:
+                line = f.read()
+                line = line.replace(f"EXAMPLE", str(EseInitials))
                 writeFile = open(file_path, "w")
                 writeFile.write(line)
 
