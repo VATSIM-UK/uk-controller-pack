46f7154e93c4c1daf2c6d91f9dd4b28572a411c7
luke11brown <13762210+luke11brown@users.noreply.github.com>
2023-09-24 11:07:02 +0400

    Improve LogonDetails.py comments for readability

--- UK/LogonDetails.py
+++ UK/LogonDetails.py
@@ -3,15 +3,22 @@ import time
 
 # User Inputs
 # Name
+
 Name = input("Enter Name: ")
 os.system('cls')
+
 #Initials (used for ESE alternate ownership and OBS callsign)
+
 Initials = input("Enter Initials (for _OBS callsign):")
 os.system('cls')
+
 # VATSIM Certificate ID
+
 CID = input("Enter CID: ")
 os.system('cls')
+
 #Controller Rating
+
 options = ['OBS', 'S1', 'S2', 'S3', 'C1', 'C2 (not used)', 'C3',  'I1', 'I2 (not used)', 'I3', 'SUP', 'ADM']
 
 rating = ''
@@ -26,11 +33,14 @@ input_message += 'Your choice: '
 while rating not in map(str, range(0, len(options))):
     rating = input(input_message)
 os.system('cls')
+
+# Password WARNING:EuroScope stores passwords as plain text!
+
 Password = input("Enter Password: ")
 os.system('cls')
-# Left commented as password is stored plain text, could be uncommented later
 
-#Hoppie CPDLC logon code
+# Hoppie CPDLC logon code
+
 CPDLC = input("Enter Hoppie's Code: ")
 os.system('cls')
 
@@ -64,6 +74,9 @@ for root, dirs, files in os.walk(os.getcwd()):
             file_path = os.path.join(root, file)
             with open(file_path, 'a') as f:
                 f.write(f"\n{PrfVCCS}\n{PrfName}\n{PrfCID}\n{PrfOBSCallsign}\n{Prfrating}\n")
+
+# Adds CPDLC code to Plugins.txt files for vSMR use
+
         elif file.endswith("Plugins.txt"):
             file_path = os.path.join(root, file)
             with open(file_path, 'r') as f:
@@ -71,6 +84,9 @@ for root, dirs, files in os.walk(os.getcwd()):
                 line = line.replace("END", "vSMR:cpdlc_password:"+CPDLC+"\nEND")
                 writeFile = open(file_path, "w")
                 writeFile.write(line)
+
+# Adds Intials to ESE for use with alternate ownership
+
         elif file.endswith(".ese"):
             file_path = os.path.join(root, file)
             with open(file_path, 'r') as f:
