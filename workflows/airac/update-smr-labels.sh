004b203f144587be577348fa2e08f6e50094fed4
luke11brown <13762210+luke11brown@users.noreply.github.com>
2025-02-23 10:20:32 +0400

    Update update-smr-labels.sh

--- workflows/airac/update-smr-labels.sh
+++ workflows/airac/update-smr-labels.sh
@@ -8,28 +8,30 @@ TMPD=$(mktemp -d)
 
 for SCT in UK/Data/Sector/*.sct; do
     BAS=$(basename "$SCT")
+    echo "Processing $SCT..."
 
     OFS=$(grep -nF "[LABELS]" "$SCT" | cut -d: -f1 || echo "")
     if [ -z "$OFS" ]; then
         echo "No [LABELS] found in $SCT"
         continue
     fi
-    tail -n +$OFS "$SCT" | tail -n +2 > $TMP1
+    tail -n +"$OFS" "$SCT" | tail -n +2 > "$TMP1"
 
-    LEN=$(grep -nE "^\[.+\]$" $TMP1 | cut -d: -f1 || echo "")
+    LEN=$(grep -nE "^\[.+\]$" "$TMP1" | cut -d: -f1 || echo "")
     if [ -z "$LEN" ]; then
         echo "No section found after [LABELS] in $SCT"
         continue
     fi
-    head -n $LEN $TMP1 | grep -oE '^".+"\s' | cut -d\" -f2 | LC_ALL=C sort -fu > $TMP2
+    head -n "$LEN" "$TMP1" | grep -oE '^".+"\s' | cut -d\" -f2 | LC_ALL=C sort -fu > "$TMP2"
 
-    LEN=$(wc -l < $TMP2)
-    yes "Free Text:SCT2" | head -n $LEN | paste -d\\\\ - $TMP2 > $TMP1
-    yes "freetext" | head -n $LEN | paste -d: $TMP1 - > "$TMPD/$BAS"
+    LEN=$(wc -l < "$TMP2")
+    yes "Free Text:SCT2" | head -n "$LEN" | paste -d\\\\ - "$TMP2" > "$TMP1"
+    yes "freetext" | head -n "$LEN" | paste -d: "$TMP1" - > "$TMPD/$BAS"
 done
 
 grep -lrF "SMR radar display" --include=*.asr | while read -r ASR; do
     ASD=${ASR#*/}
+    echo "Processing $ASR..."
     PRF=$(grep -lrF "${ASD//\//\\}" --include=*.prf | head -n1 || echo "")
     if [ -z "$PRF" ]; then
         echo "No .prf file found for $ASD"
@@ -42,17 +44,17 @@ grep -lrF "SMR radar display" --include=*.asr | while read -r ASR; do
     fi
     SCT=${SCT##*\\}
 
-    grep -vF "Free Text:SCT2\\" "$ASR" > $TMP1
+    grep -vF "Free Text:SCT2\\" "$ASR" > "$TMP1"
     OFS=$(grep -m1 -nF "Free Text:SCT2\\" "$ASR" | cut -d: -f1 || echo "")
     if [ -z "$OFS" ]; then
         echo "No 'Free Text:SCT2\\' found in $ASR"
         continue
     fi
 
-    head -n $OFS $TMP1 | head -n -1 > "$ASR"
+    head -n "$OFS" "$TMP1" | head -n -1 > "$ASR"
     cat "$TMPD/$SCT" >> "$ASR"
-    tail -n +$OFS $TMP1 >> "$ASR"
+    tail -n +"$OFS" "$TMP1" >> "$ASR"
 done
 
-rm $TMP1 $TMP2
-rm -r $TMPD
+rm "$TMP1" "$TMP2"
+rm -r "$TMPD"
