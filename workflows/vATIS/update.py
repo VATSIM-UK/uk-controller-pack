b5c9107e0cea845752bb48b8ca825d5c819ae66e
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2025-04-08 12:22:38 +0100

    update script for testing

--- workflows/vATIS/update.py
+++ workflows/vATIS/update.py
@@ -2,6 +2,19 @@ import os
 import json
 import sys
 from datetime import datetime
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Update vATIS serials")
+    parser.add_argument('--changed-files-list', required=True,
+                        help='List of changed files between commits')
+    args = parser.parse_args()
+    with open(args.changed_files_list, 'r')as f:
+        changes_files = f.read().splitlines()
+
+    print(changes_files)
+
 
 input_dir = os.path.abspath('_vATIS')
 
@@ -13,48 +26,52 @@ print(filenames_to_update)
 # If no filenames are provided, update all JSON files in the input directory
 
 if not filenames_to_update:
-    filenames_to_update = [f for f in os.listdir(input_dir) if f.endswith('.json')]
-    
+    filenames_to_update = [f for f in os.listdir(
+        input_dir) if f.endswith('.json')]
+
 for filename in filenames_to_update:
     if filename.endswith('.json'):
         name = os.path.splitext(filename)[0]
         input_filepath = os.path.join(input_dir, filename)
-        
+
         if not os.path.exists(input_filepath):
             print(f"File {input_filepath} does not exist.")
             continue
-        
+
         with open(input_filepath, 'r') as file:
             data = json.load(file)
-        
+
         current_serial = str(data.get("updateSerial", "1970010100"))
-        
+
         current_date = current_serial[:8]
         current_serial_number = int(current_serial[8:])
-        
+
         today_date = datetime.now().strftime("%Y%m%d")
 
-        print(f"{current_date = }")
-        print(f"{today_date = }")
-        print(f"{current_serial_number = }")
-        
+        print(f"{current_date=}")
+        print(f"{today_date=}")
+        print(f"{current_serial_number=}")
+
         if current_date == today_date:
             new_serial_number = current_serial_number + 1
         else:
             new_serial_number = 1
 
-        if new_serial_number > 99: # should never be an issue
+        if new_serial_number > 99:  # should never be an issue
             print("There have been too many updates today. The script will now exit")
             sys.exit(1)
-        
+
         update_serial = int(f"{today_date}{new_serial_number:02d}")
-        
+
         update_url = f"https://raw.githubusercontent.com/VATSIM-UK/uk-controller-pack/refs/heads/main/_vATIS/{name}.json"
-        
+
         data["updateUrl"] = update_url
         data["updateSerial"] = update_serial
-        
+
         with open(input_filepath, 'w') as file:
             json.dump(data, file, indent=2)
-        
+
         print(f"Updated {filename} with new serial {update_serial}")
+
+if __name__ == "__main__":
+    main()
