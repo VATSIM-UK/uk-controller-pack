51e614eb2975412064f8faffaa4ea6e27ed3cb11
luke11brown <13762210+luke11brown@users.noreply.github.com>
2025-12-29 18:16:23 +0400

    """

--- workflows/build-updater/Updater.py
+++ workflows/build-updater/Updater.py
@@ -518,7 +518,7 @@ class UpdaterApp:
 if __name__ == "__main__":
     if "--print-build" in sys.argv:
         print(UPDATER_BUILD)
-        raise SystemExit(0)"
+        raise SystemExit(0)
 
     root = tk.Tk()
     app = UpdaterApp(root)
